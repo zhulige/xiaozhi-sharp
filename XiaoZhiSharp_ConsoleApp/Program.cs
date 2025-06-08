388262fa54a7d1733f1d007b271d908e3a65d1be
朱利戈 <zhulige@hotmail.com>
2025-06-08 23:37:01 +0800

    修改排版

--- XiaoZhiSharp_ConsoleApp/Program.cs
+++ XiaoZhiSharp_ConsoleApp/Program.cs
@@ -29,18 +29,18 @@ class Program
         Console.ForegroundColor = ConsoleColor.Green;
         Console.Title = "小智XiaoZhiSharp客户端";
         string logoAndCopyright = @"
-        ========================================================================
-        欢迎使用“小智XiaoZhiSharp客户端” ！
+========================================================================
+欢迎使用“小智XiaoZhiSharp客户端” ！
 
-        当前功能：
-        1. 语音消息 输入回车：开始录音；再次输入回车：结束录音
-        2. 文字消息 可以随意输入文字对话
-        3. 全量往返协议输出，方便调试
+当前功能：
+1. 语音消息 输入回车：开始录音；再次输入回车：结束录音
+2. 文字消息 可以随意输入文字对话
+3. 全量往返协议输出，方便调试
 
-        要是你在使用中有啥想法或者遇到问题，别犹豫，找我们哟：
-        微信：Zhu-Lige       电子邮箱：zhuLige@qq.com
-        有任何动态请大家关注 https://github.com/zhulige/xiaozhi-sharp
-        ========================================================================";
+要是你在使用中有啥想法或者遇到问题，别犹豫，找我们哟：
+微信：Zhu-Lige       电子邮箱：zhuLige@qq.com
+有任何动态请大家关注 https://github.com/zhulige/xiaozhi-sharp
+========================================================================";
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(logoAndCopyright);
         Console.ForegroundColor = ConsoleColor.Red;
