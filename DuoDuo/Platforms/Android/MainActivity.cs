d2c3b9f737c6be07052e82dd7ec6e81bd32221d2
朱利戈 <zhulige@hotmail.com>
2025-06-23 19:52:35 +0800

    打包发布

--- DuoDuo/Platforms/Android/MainActivity.cs
+++ DuoDuo/Platforms/Android/MainActivity.cs
@@ -1,11 +1,41 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using System.Text;
 
 namespace DuoDuo
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // 获取 Android ID 并格式化为 MAC 地址格式
+            var androidId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            var formattedAndroidId = FormatAndroidIdToMacFormat(androidId);
+            Global.DeviceId = formattedAndroidId;
+
+            base.OnCreate(savedInstanceState);
+        }
+
+        // 将 Android ID 格式化为 MAC 地址格式
+        public string FormatAndroidIdToMacFormat(string androidId)
+        {
+            if (string.IsNullOrEmpty(androidId))
+            {
+                return string.Empty;
+            }
+
+            StringBuilder formattedId = new StringBuilder();
+            for (int i = 0; i < 12; i++)
+            {
+                formattedId.Append(androidId[i]);
+                if ((i + 1) % 2 == 0 && i < 12 - 1)
+                {
+                    formattedId.Append(":");
+                }
+            }
+            return formattedId.ToString();
+        }
     }
 }
